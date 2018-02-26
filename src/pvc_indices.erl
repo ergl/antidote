b8f664f673b9204a8dd177acb86a1dfb22a717dc
ergl <ergl@users.noreply.github.com>
2018-02-26 12:14:34 +0100

    Add helper functions for claimed indices

--- src/pvc_indices.erl
+++ src/pvc_indices.erl
@@ -22,6 +22,8 @@
 
 -include("antidote.hrl").
 
+-define(CLAIMED, claimed).
+
 %% API
 -export([u_index/4,
          read_u_index/3,
@@ -38,11 +40,11 @@ read_u_index(IndexName, IndexValue, TxId) ->
 index(IndexName, IndexValue, RefKey, TxId) ->
     RootKey = make_u_index_key(IndexName, IndexValue),
     IndexKey = make_index_key(IndexName, IndexValue, RefKey),
-    {ok, [Val]} = pvc:read_keys(RootKey, TxId),
-    Updates = case Val of
-        <<>> ->
-            [{RootKey, taken}, {IndexKey, RefKey}];
-        taken ->
+    Updates = case claimed_index(RootKey, TxId) of
+        false ->
+            [{RootKey, ?CLAIMED}, {IndexKey, RefKey}];
+
+        true ->
             [{IndexKey, RefKey}]
     end,
     update_indices(Updates, TxId).
@@ -50,6 +52,12 @@ index(IndexName, IndexValue, RefKey, TxId) ->
 update_indices(Updates, TxId = #tx_id{server_pid = Pid}) ->
     ok = pvc:update_keys(Updates, TxId),
     gen_fsm:sync_send_event(Pid, {pvc_index, Updates}, ?OP_TIMEOUT).
+
+%% Util functions
+
+claimed_index(RootKey, TxId) ->
+    {ok, [RootVal]} = pvc:read_keys(RootKey, TxId),
+    RootVal =:= ?CLAIMED.
 
 %% TODO(borja): Handle non-binary data
 make_u_index_key(IndexName, IndexValue) ->
