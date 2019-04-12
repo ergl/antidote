5d6286700e122a08b3e9b17f47fad86aee36134a
ergl <ergl@users.noreply.github.com>
2019-04-12 12:37:07 +0200

    Use packet, 4

--- src/coord_pb_server_worker.erl
+++ src/coord_pb_server_worker.erl
@@ -43,7 +43,7 @@ start_link(Ref, Socket, Transport, Opts) ->
 init({Ref, Socket, Transport, _Opts}) ->
     ok = ranch:accept_ack(Ref),
     ok = ranch:remove_connection(Ref),
-    ok = Transport:setopts(Socket, [{active, once}, {packet, 2}]),
+    ok = Transport:setopts(Socket, [{active, once}, {packet, 4}]),
     gen_server:enter_loop(?MODULE, [], #state{socket=Socket, transport=Transport}).
 
 handle_call(E, _From, S) ->
