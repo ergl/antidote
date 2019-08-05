1aa22c8f5c7a3488e7796a903129f9521fe7364f
ergl <ergl@users.noreply.github.com>
2019-08-05 13:39:54 +0200

    Add documentation to PVC log limits

--- include/pvc.hrl
+++ include/pvc.hrl
@@ -8,7 +8,15 @@
 -define(UINDEX_SEP, <<"%">>).
 -define(INDEX_PAGE_LIMIT, 25).
 
-%% Amount of Transaction metadata kept
+%% Amount of Transaction metadata kept for the Version and Commit log
+%%
+%% VERSION_THRESHOLD specifies the point where the logs will perform a GC
+%% The lower this number is set, the more common GC will become.
+%%
+%% MAX_VERSIONS specifies the number of versions kept in the logs after a GC
+%%
+%% So, every time VERSION_THRESHOLD versions are reached, we will remove all
+%% versions, except for last MAX_VERSIONS.
 -define(VERSION_THRESHOLD, 500).
 -define(MAX_VERSIONS, 100).
 
