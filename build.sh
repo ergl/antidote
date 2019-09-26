0de61fd92cb7d7b2d654c3cef22bbd404c1c6840
ergl <ergl@users.noreply.github.com>
2019-09-26 14:07:15 +0200

    Add local build script

--- build.sh
+++ build.sh
@@ -1,1 +1,35 @@
-make test
+#!/usr/bin/env bash
+
+main() {
+    local command="${1}"
+    local profile="${2:-default}"
+
+    case "${command}" in
+        "compile")
+            ./rebar3 as "${profile}" compile
+            ./rebar3 as "${profile}" release -n antidote
+            ./change_script.sh _build/"${profile}"/rel/antidote/bin/antidote
+            ;;
+        "run")
+            _build/"${profile}"/rel/antidote/bin/env start
+            sleep 2
+            _build/"${profile}"/rel/antidote/bin/env ping
+            ;;
+        "stop")
+            _build/"${profile}"/rel/antidote/bin/env stop
+            ;;
+        "restart")
+            _build/"${profile}"/rel/antidote/bin/env stop
+            rm -rf _build/"${profile}"/rel
+            ./rebar3 as "${profile}" compile
+            ./rebar3 as "${profile}" release -n antidote
+            ./change_script.sh _build/"${profile}"/rel/antidote/bin/antidote
+            _build/"${profile}"/rel/antidote/bin/env start
+            sleep 2
+            _build/"${profile}"/rel/antidote/bin/env ping
+            ;;
+
+    esac
+}
+
+main "$@"
